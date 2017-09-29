0b2ed622ecaf2812a3f49bb43008eebdd842234f
nabuckein <pauri@hotmail.com>
2017-09-28 21:26:08 -0500

    Add files via upload

--- Gallery.js
+++ Gallery.js
@@ -4,8 +4,12 @@ import React, { Component } from 'react';
 var image1 = require('./images/clock1.png');
 var image2 = require('./images/clock2.png');
 var image3 = require('./images/clock3.png');
+var image4 = require('./images/cuttingBoard4.jpg');
+var image5 = require('./images/cuttingBoard5.png');
+var image6 = require('./images/cuttingBoard6.png');
+var image7 = require('./images/cuttingBoard7.png');
 
-var imagesArr = [image1,image2,image3];
+var imagesArr = [image1,image2,image3,image4,image5,image6,image7];
 var imageNumber = 0;
 
 class Gallery extends Component {
@@ -14,7 +18,10 @@ class Gallery extends Component {
 	    super(props);
 	    this.state={
 
-	      	imgToDisplay:imagesArr[imageNumber]
+	      	imgToDisplay:imagesArr[imageNumber],
+	      	listingsObj:window.listingsObj,
+	      	itemToDisplay:0
+
 	    }
 	  }
 
@@ -27,7 +34,7 @@ class Gallery extends Component {
 			imageNumber=0;
 		}
 		
-		this.setState({imgToDisplay:imagesArr[imageNumber]});
+		this.setState({imgToDisplay:imagesArr[imageNumber],itemToDisplay:imageNumber});
 	}
 	toPreviousImage=(e)=>{
 		if(imageNumber>0){
@@ -35,37 +42,40 @@ class Gallery extends Component {
 		}else{
 			imageNumber=imagesArr.length-1;
 		}
-		this.setState({imgToDisplay:imagesArr[imageNumber]});
+		this.setState({imgToDisplay:imagesArr[imageNumber],itemToDisplay:imageNumber});
 	}
 
     render() {
-    	console.log(imageNumber);
+    	
+    	console.log(window.listingsObj);
+
       return (
+
         <div className="Gallery" style={GalleryStyles}>
-      	    
-            <div className="linkTitles">
+
+            {/*<div className="linkTitles">
               <p className="linkTitlesText">Serving/Cutting Boards</p>
               <p className="linkTitlesText">Table Top</p>
               <p className="linkTitlesText">Wall Decor</p>
               <p className="linkTitlesText">Clocks</p>
               <p className="linkTitlesText">Paintings</p>
-            </div>
+            </div>*/}
       	    <div className="imageAndArrows">
-              <p className="imageTitle">Red Pointer Clock</p>
+              <p className="imageTitle">{this.state.listingsObj[this.state.itemToDisplay].title}</p>
       	    	<i className="fa fa-arrow-circle-left" aria-hidden="true" onClick={this.toNextImage}></i>
-        		  <img alt="test1" className="images" src={this.state.imgToDisplay} style={imagesStyle}></img>
-        		  <i className="fa fa-arrow-circle-right" aria-hidden="true" onClick={this.toPreviousImage}></i>
-              <p className="imageDescriptionLine1">Diameter: 50cm (19.7")</p>
-        	  </div>
-            <div className="linkDescriptions">
+        		<img alt="test1" className="images" src={this.state.imgToDisplay} style={imagesStyle}></img>
+        		<i className="fa fa-arrow-circle-right" aria-hidden="true" onClick={this.toPreviousImage}></i>
+              	{/*<p className="imageDescriptionLine1">Diameter: 50cm (19.7")</p>*/}
+              	<p className="imageDescriptionLine2">https://www.etsy.com/listing/546585978/small-serving-or-cutting-board?ref=shop_home_active_1</p>
+        	</div>
+            {/*<div className="linkDescriptions">
               <p className="linkDescriptionsText">These items can be used as a serving tray as well as a cutting board. Wall decor ideas are also available in matching colors</p>
-            </div>
+            </div>*/}
        		
       	</div>
     );
   }
 }
-
 export default Gallery;
 
 const GalleryStyles = {
@@ -84,12 +94,12 @@ const GalleryStyles = {
   marginTop:'20px',
   marginBottom:'20px'
 }
-
 const imagesStyle = {
-	animation: 'opacitychange 2.5s',
+  animation: 'opacitychange 2.5s',
   borderRadius: '10px',
-  width:'60%',
-  height: '50%',
+  width: '40%',
+  height: '400px',
   marginLeft:'20px',
-  marginRight:'20px'
+  marginRight:'20px',
+  overflow:'scroll'
 }
